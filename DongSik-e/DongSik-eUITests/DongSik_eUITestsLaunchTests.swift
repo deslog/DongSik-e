f6038c2f0dd8d753f0c637c996b9c0fd8b11044b
JI SOO <dlwltn815@gmail.com>
2023-01-24 15:05:22 +0900

    [SETTING] xcode 프로젝트 생성(#2)

---
+++ DongSik-e/DongSik-eUITests/DongSik_eUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DongSik_eUITestsLaunchTests.swift
+//  DongSik-eUITests
+//
+//  Created by LeeJiSoo on 2023/01/24.
+//
+
+import XCTest
+
+class DongSik_eUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
