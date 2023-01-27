79d851652b6fbbdd36e041a0910e732d4ee3d3be
JI SOO <dlwltn815@gmail.com>
2023-01-27 15:32:55 +0900

    [ADD] UIFont 익스텐션 추가(#9)

---
+++ DongSik-e/DongSik-e/Global/Extensions/UIFont+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIFont+Extension.swift
+//  DongSik-e
+//
+//  Created by LeeJiSoo on 2023/01/27.
+//
+
+import UIKit
+
+extension UIFont {
+    static func preferredFont(for style: TextStyle, weight: Weight) -> UIFont {
+        let traits = UITraitCollection(preferredContentSizeCategory: .large)
+        let desc = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style, compatibleWith: traits)
+        let font = UIFont.systemFont(ofSize: desc.pointSize, weight: weight)
+        let metrics = UIFontMetrics(forTextStyle: style)
+        return metrics.scaledFont(for: font)
+    }
+}
