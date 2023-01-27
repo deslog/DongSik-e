9055826d40f92e7c9083523c71ec00752f09c946
JI SOO <dlwltn815@gmail.com>
2023-01-27 14:37:17 +0900

    [ADD] 탭바 진입점 SchoolMealVC으로 변경(#9)

---
+++ DongSik-e/DongSik-e/Screen/SchoolMeal/SchoolMealViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SchoolMealViewController.swift
+//  DongSik-e
+//
+//  Created by LeeJiSoo on 2023/01/27.
+//
+
+import UIKit
+
+class SchoolMealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
