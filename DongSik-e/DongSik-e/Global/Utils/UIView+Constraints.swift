541625165f957d169f0483e71ef7de7d7762b68a
JI SOO <dlwltn815@gmail.com>
2023-02-01 19:23:55 +0900

    [FEAT] UIView의 height와 width를 쉽게 지정해주는 uitils추가(#9)

--- DongSik-e/DongSik-e/Global/Utils/UIView+Constraints.swift
+++ DongSik-e/DongSik-e/Global/Utils/UIView+Constraints.swift
@@ -5,4 +5,28 @@
 //  Created by LeeJiSoo on 2023/01/24.
 //
 
-import Foundation
+import UIKit
+
+enum Frame {
+    case widthAnchor, heightAnchor
+}
+
+extension UIView {
+    @discardableResult
+    func constraint(_ anchor: Frame, constant: CGFloat) -> [Frame: NSLayoutConstraint] {
+        var constraints: [Frame: NSLayoutConstraint] = [:]
+
+        self.translatesAutoresizingMaskIntoConstraints = false
+
+        switch anchor {
+        case .widthAnchor:
+            constraints[.widthAnchor] = self.widthAnchor.constraint(equalToConstant: constant)
+        case .heightAnchor:
+            constraints[.heightAnchor] = self.heightAnchor.constraint(equalToConstant: constant)
+        }
+
+        let constraintsArray: [NSLayoutConstraint] = Array(constraints.values)
+        NSLayoutConstraint.activate(constraintsArray)
+        return constraints
+    }
+}
